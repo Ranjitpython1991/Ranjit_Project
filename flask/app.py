974ae06d27e08ca377e6d065993967acce326519
Ranjit <Ranjitpython1991@gmail.com>
2023-07-31 23:11:33 +0530

    sum and avrage flaks

--- flask/app.py
+++ flask/app.py
@@ -1,6 +1,6 @@
 
 
-from flask import Flask ,render_template,redirect
+from flask import Flask ,render_template,redirect,request,url_for,jsonify
 
 
 app=Flask(__name__)
@@ -18,7 +18,38 @@ def index():
 def success(score) :
     return "your are pass and your score is : "+ score
 
+@app.route('/fail/<score>')
+def fail(score) :
+    return "your are fail and your score is : "+ score
 
+
+@app.route('/form',methods=["GET","POST"])
+def form():
+    if request.method== "GET":
+        return render_template('form.html')
+    else :
+        maths=float(request.form['maths'])
+        science=float(request.form['science'])
+        history=float(request.form['history'])
+
+        ave_mark=(maths+science+history)/3
+        #return render_template('form.html',score=ave_mark)
+        res=" "
+        if ave_mark>35:
+            res="success"
+        else :
+            res="fail"
+        return redirect(url_for(res,score=ave_mark))
+    
+@app.route('/api',methods=["GET","POST"])
+def api():
+    data=request.get_json()
+    print(data)
+    maths=float(data['maths'])
+    science=float(data['science'])
+    history=float(data['history'])
+    return jsonify('sum :',maths+science+history)
+    
 
 
 
